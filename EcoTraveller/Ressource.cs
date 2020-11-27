283689e0862822d180c7c6dd5f34defca7d5de59
Di Cosola Luca <DiCosolaLu@s2.rpn.ch>
2020-11-27 12:31:47 +0100

    third commit

---
+++ EcoTraveller/Ressource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chat
+{
+    class Ressource
+    {
+        public static BitmapImage getImage(Byte[] bytes)
+        {
+            var stream = new MemoryStream(bytes);
+            stream.Seek(0, SeekOrigin.Begin);
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = stream;
+            image.EndInit();
+            return image;
+        }
+        public static void fillRect(Rectangle rect, BitmapImage image)
+        {
+            rect.Fill = new ImageBrush { ImageSource = image };
+        }
+    }
+}
